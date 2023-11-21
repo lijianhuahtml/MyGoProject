3d6e8f4aa5a3be279ba915ccfdf14bf3f0b2885c
lijianhuahtml <481802114@qq.com>
2023-11-21 23:12:51 +0800

    home

--- grpc/server/main.go
+++ grpc/server/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	pb "MyGoProject/grpc/server/proto"
 	"context"
+	"errors"
+	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 	"net"
 )
 
@@ -12,6 +16,24 @@ type server struct {
 }
 
 func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errors.New("未传输token")
+	}
+	var appId string
+	var appKey string
+	if v, ok := md["appid"]; ok {
+		appId = v[0]
+	}
+	if v, ok := md["appkey"]; ok {
+		appKey = v[0]
+	}
+	fmt.Println(appId, appKey)
+	if appId != "lijianhua" || appKey != "123" {
+		return nil, errors.New("token 不正确")
+	}
+
 	return &pb.HelloResponse{ResponseMsg: "hello," + req.RequestName}, nil
 }
 
@@ -19,7 +41,7 @@ func main() {
 	// 开启端口
 	listen, _ := net.Listen("tcp", ":8088")
 	// 创建grpc服务
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
 	// 在grpc服务端中注册我们自己写的服务
 	pb.RegisterSayHelloServer(grpcServer, &server{})
 
